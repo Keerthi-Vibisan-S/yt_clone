5aa7eaf5186c2b34452e5e0df72a36881bd90bd3
KEERTHI VIBISAN S <keerthivibisan.ct20@bitsathy.ac.in>
2022-01-20 21:36:06 +0530

    Channel video Upload Added

--- src/components/ChannelVideoUpload.js
+++ src/components/ChannelVideoUpload.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import * as ai from 'react-icons/ai';
+import Axios from 'axios';
 
 export default function ChannelVideoUpload(props)
 {
-    console.log(props);
+    //console.log(props);
     const {Sno, channel} = props;
-    console.log(channel," -- channel");
+    //console.log(channel," -- channel");
     const d = new Date();    
     const [data, setData] = useState(
     {
@@ -14,19 +15,67 @@ export default function ChannelVideoUpload(props)
         vname: "",
         desc: "",
         vtype: "",
-        videofile: null,
-        date:  String (d.getUTCDate()+'/'+(d.getUTCMonth()+1)+"/"+d.getUTCFullYear())
+        date:  String (d.getUTCDate()+'/'+(d.getUTCMonth()+1)+"/"+d.getUTCFullYear()),
+        vpath: ""
     }
     );
+
+    const[file, setFile] = useState();
+    const [progress, setProgress] = useState(0);
 
     useEffect(() => {
         setData({...data, Cno: props.channel})
     }, [props.channel])
-    console.log(data.Cno+ "-- data CNO");
+    //console.log(data.Cno+ "-- data CNO");
 
     const uploadData = async (e) => {
         e.preventDefault();
-        console.log(data);
+        const formdata = new FormData();
+        formdata.append("vfile", file);
+        
+
+        try
+        {
+            const url = "http://localhost:2022/video/upload";
+            let res = await Axios.post(url, formdata, {
+                headers:{
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: (progressEvent) => {
+                    setProgress(parseInt(Math.round(progressEvent.loaded * 100)/progressEvent.total))
+                }
+            });
+
+            if(res.data)
+            {
+                console.log(res.data);
+                setData({...data, vpath: String(res.data)});
+                console.log(data);
+                const url = "http://localhost:2022/video/updateDb";
+
+                let res2 = await Axios.post(url, data);
+
+                if(res2.data)
+                {
+                    setData({
+                        Cno: channel,
+                        Sno: parseInt(Sno),
+                        vname: "",
+                        desc: "",
+                        vtype: "",
+                        date:  String (d.getUTCDate()+'/'+(d.getUTCMonth()+1)+"/"+d.getUTCFullYear()),
+                        vpath: ""
+                    });
+
+                    setFile(null);
+                    setTimeout(() => setProgress(0) , 5000);
+                }
+            }
+        }
+        catch(err)
+        {
+            console.log("Error Occurred whille Axios Req "+err);
+        }
     }
 
   return (
@@ -49,7 +98,7 @@ export default function ChannelVideoUpload(props)
                 
                             <div className="mb-3">
                                 <label htmlFor="Name" className="form-label">Content Name</label>
-                                <input className="form-control" type="text" id="Name" value={data.vname} onChange={(event) => setData({...data, cname: event.target.value})}/>
+                                <input className="form-control" type="text" id="Name" value={data.vname} onChange={(event) => setData({...data, vname: event.target.value})}/>
                             </div>
 
                             <div className="mb-3">
@@ -69,7 +118,7 @@ export default function ChannelVideoUpload(props)
 
                             <div className="mb-3">
                                 <label htmlFor="formFile" className="form-label">Choose the Video File</label>
-                                <input className="form-control" type="file" id="formFile" onChange={(e) => setData({...data, videofile: e.target.files[0]})} />
+                                <input className="form-control" type="file" id="formFile" onChange={(e) => setFile(e.target.files[0])} />
                             </div>
 
                             <div className="mb-3">
@@ -80,7 +129,9 @@ export default function ChannelVideoUpload(props)
                             <div className='mb-3'>
                                 <button className='btn btn-outline-success' type='submit'>Upload</button>
                             </div>
-
+                            <div className="progress">
+                            <div className="progress-bar" role="progressbar" style={{width: `${progress}%`}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{`${progress}%`}</div>
+                            </div>
                         </form>
                     </div>
                     <div className="modal-footer">
