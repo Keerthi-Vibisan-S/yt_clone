7dc91e01e21064f9d49559ba9f51a2596c20cf79
Keerthi Vibisan <keerthivibisan.ct20@bitsathy.ac.in>
2022-02-20 18:50:00 +0530

    🔐 Google Authentication Backend added and ⚠️  Checking with our Database Pending

--- src/Routes/SigninPage.js
+++ src/Routes/SigninPage.js
@@ -5,18 +5,39 @@ import * as fc from 'react-icons/fc';
 
 export default function SigninPage() {
     const[auth, setAuth] = useState(null);
+    const [danger, setDanger] = useState(false);
 
     async function responseGoogle(response)
     {
         //Setting login cred in local storage
-        localStorage.setItem('authDetails', JSON.stringify(response.profileObj));
+        //localStorage.setItem('authDetails', JSON.stringify(response.profileObj));
         setAuth(response.profileObj);
 
         const url = `http://localhost:2022/auth/details/${response.profileObj.email}`;
         let all = await Axios.get(url);
-        localStorage.setItem("userSno", JSON.stringify(all.data[0]));
+        //localStorage.setItem("userSno", JSON.stringify(all.data[0]));
 
-        window.location.reload();
+        //TODO: Sending googles token to backend for verification
+        const url2 = 'http://localhost:2023/getToken';
+        let res = await Axios.post(url2, {token: response.tokenId});
+        
+        if(res.data)
+        {
+          if(response.profileObj.email == res.data.email && res.data.emailVerify)
+          {
+            //! HERE WE MUST CHECK with our database and register
+
+            localStorage.setItem('authDetails', JSON.stringify(response.profileObj));
+            localStorage.setItem("userSno", JSON.stringify(all.data[0]));
+            window.location.reload();
+          }
+
+          else
+          {
+            setDanger(true);
+          }
+        }
+
     }
 
   return (
@@ -34,6 +55,10 @@ export default function SigninPage() {
             cookiePolicy={'single_host_origin'}
             /></div>
             <p className='text-muted'>Sign In by Clicking the Icon</p>
+
+            {danger?<div>
+              <p className='text-danger'>Un - Authorized</p>
+            </div>: ""}
   </section>
 )
 }
